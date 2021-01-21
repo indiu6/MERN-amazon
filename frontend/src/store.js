aed7dc519754f42d97890a258bb0294699b06264
indiu6 <hewas6@gmail.com>
2021-01-21 18:15:42 -0500

    edit redux store

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,13 +1,19 @@
-import { applyMiddleware, compose, createStore } from 'redux';
-import data from './data';
+import { applyMiddleware, combineReducers, compose, createStore } from 'redux';
 import thunk from 'redux-thunk';
+import {
+  productDetailsReducer,
+  productListReducer,
+} from './reducers/productReducers';
 
 const initialState = {};
-const reducer = (state, action) => {
-  return { products: data.products };
-};
+
+const reducer = combineReducers({
+  productList: productListReducer,
+  productDetails: productDetailsReducer,
+});
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 const store = createStore(
   reducer,
   initialState,
